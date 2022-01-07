17937cbb56688ab4b8b3c0fff086234594d0358c
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-07 21:25:16 +0900

    テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :nickname, presence: true
+  # validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  # validates :password, presence: true, length: { minimum: 6 }, confirmation: true
+  # validates :encrypted_password, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :birthday, presence: true
+
+  has_many :items
+  has_many :orders
 end
