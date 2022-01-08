bf7fef64aa67eff2e5c9f108c0b352c7409e4f1a
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-08 16:17:36 +0900

    itemテーブルにカラムの追加

---
+++ db/migrate/20220108065024_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.integer :category_id
+      t.integer :status_id
+      t.integer :delivery_charge_id
+      t.integer :prefecture_id
+      t.integer :scheduled_id
+      t.integer :price
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_054811) do
+ActiveRecord::Schema.define(version: 2022_01_08_065024) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "category_id"
+    t.integer "status_id"
+    t.integer "delivery_charge_id"
+    t.integer "prefecture_id"
+    t.integer "scheduled_id"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
