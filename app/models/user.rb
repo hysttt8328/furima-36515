acf511283b2fe088c38e7440406620d0a1103e55
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-08 14:53:00 +0900

    テストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
@@ -18,12 +17,9 @@ class User < ApplicationRecord
   end
 
   validates :nickname, presence: true
-  # validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  # validates :password, presence: true, length: { minimum: 6 }, confirmation: true
-  # validates :encrypted_password, presence: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :birthday, presence: true
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 end
