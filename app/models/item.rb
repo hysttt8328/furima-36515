befda3d5de76388976d484e7204ae3a076c01043
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-09 18:09:54 +0900

    商品モデルにバリテーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_id , presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+
+
+
+
+
+
+
   has_one_attached :image
 end
