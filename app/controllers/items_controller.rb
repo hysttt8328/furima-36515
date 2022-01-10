14c02bf766d06a6a3e984ffa128ba8bd72adad44
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-10 12:18:53 +0900

    ユーザー認証機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,17 +9,14 @@ class ItemsController < ApplicationController
     @item = Item.new
     @item.item_imgs.new
     @category_parent = Category.where(ancestry: nil)
-      # 親カテゴリーが選択された後に動くアクション
     def get_category_child
       @category_child = Category.find("#{params[:parent_id]}").children
       render json: @category_child
-      #親カテゴリーに紐付く子カテゴリーを取得
     end
-      # 子カテゴリーが選択された後に動くアクション
+
     def get_category_grandchild
       @category_grandchild = Category.find("#{params[:child_id]}").children
       render json: @category_grandchild
-      #子カテゴリーに紐付く孫カテゴリーの配列を取得
     end
   end
 
@@ -40,4 +37,3 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :brand_id, :pref_id, :size_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :postage_type_id, :category_id, :trading_status, item_imgs_attributes: [:url, :id]).merge(seller_id: current_user.id)
   end
-
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,19 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :scheduled_id , presence: true
-  validates :price, presence: true
-  validates :user, presence: true
-
-
-
-
-
-
+  validate :images_presence
+  validates :name, :description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :scheduled_id,:user_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   has_one_attached :image
+  belongs_to :user
+  belongs_to :category
+
+  def images_presence
+    if images.attached?
+      if images.length > 1
+        errors.add(:image, '1枚まで投稿できます')
+      end
+    else
+      errors.add(:image, '画像がありません')
+    end
+  end
 end
