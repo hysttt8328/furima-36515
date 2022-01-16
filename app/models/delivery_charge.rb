12130e102e9739a13a0f3d3d99e3c9648feb97f5
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-16 20:36:23 +0900

    詳細ページの条件分岐

--- app/models/delivery_charge.rb
+++ app/models/delivery_charge.rb
@@ -1,8 +1,8 @@
 class DeliveryCharge < ActiveHash::Base
   self.data = [
-    { id: 1, delivery_charge_id: '--' },
-    { id: 2, delivery_charge_id: '着払い（購入者負担）' },
-    { id: 3, delivery_charge_id: '送料込み（出品者負担）' }
+    { id: 1, name: '--' },
+    { id: 2, name: '着払い（購入者負担）' },
+    { id: 3, name: '送料込み（出品者負担）' }
   ]
   include ActiveHash::Associations
   has_many :items
--- app/models/scheduled.rb
+++ app/models/scheduled.rb
@@ -1,9 +1,9 @@
 class Scheduled < ActiveHash::Base
   self.data = [
-    { id: 1, scheduled_id: '--' },
-    { id: 2, scheduled_id: '1~2日で発送' },
-    { id: 3, scheduled_id: '2~3日で発送' },
-    { id: 4, scheduled_id: '4~7日で発送' }
+    { id: 1, name: '--' },
+    { id: 2, name: '1~2日で発送' },
+    { id: 3, name: '2~3日で発送' },
+    { id: 4, name: '4~7日で発送' }
   ]
   include ActiveHash::Associations
   has_many :items
