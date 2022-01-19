29ea519729e4a0bb780d00caee83b3c6c688a299
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-19 18:03:34 +0900

    環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,7 +22,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_08c66292b01c862a82f0d682" 
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: order_params[:price],
         card: order_params[:token],
