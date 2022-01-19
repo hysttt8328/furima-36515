657a787787ad0e8aef64ed30a557be1d9f29ac5e
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-19 17:47:42 +0900

    コントローラーに決済処理の記述

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :items, only:[:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users
 
+  resources :orders, only:[:create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
