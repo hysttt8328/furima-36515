287e16745448e16599dcb43005f14053a97442e0
hysttt8328 <kamiwaru8328@gmail.com>
2022-01-29 15:18:01 +0900

    BASIC認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
   private
 
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
